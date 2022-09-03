b45aebfdc2ba8a7e46461a3e61d750fe7f584673
Cristos <cristos@pop-os.localdomain>
2022-09-04 00:08:23 +0200

    PrintModal visually done

--- components/PrintModal/PrintModal.tsx
+++ components/PrintModal/PrintModal.tsx
@@ -21,9 +21,9 @@ const PrintModal: FC<any> = ({ nftData, onClose }) => {
             <span className="inline-block ml-2 text-purple-700">PRINT</span>
           </Dialog.Title>
         </div>
-        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 xl:gap-x-8 py-12">
+        <div className="grid grid-cols-1 gap-y-10 gap-x-12 sm:grid-cols-2 py-12">
           <div role="button" tabIndex={0} key={`${nftData.id.tokenId + nftData.timeLastUpdated}FIGURE`} className="group" onClick={() => setSelectedOption('FIGURE')} onKeyUp={(e) => handleKeyUp(e, 'FIGURE')}>
-            <div className={`aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8 ${selectedOption === 'FIGURE' ? 'drop-shadow border-solid border-3 border-purple-700' : ''}`}>
+            <div className={`aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg xl:aspect-w-7 xl:aspect-h-8 ${selectedOption === 'FIGURE' ? 'drop-shadow border-solid border-3 border-purple-700' : ''}`}>
               <img
                 src={`${nftData.tokenUri.gateway}`}
                 alt="An NFT"
@@ -33,9 +33,9 @@ const PrintModal: FC<any> = ({ nftData, onClose }) => {
             <h3 className="mt-4 text-xl text-center font-semibold text-gray-200">3D Figure</h3>
           </div>
           <div role="button" tabIndex={0} key={`${nftData.id.tokenId + nftData.timeLastUpdated}CHIP`} className="group" onClick={() => setSelectedOption('CHIP')} onKeyUp={(e) => handleKeyUp(e, 'CHIP')}>
-            <div className={`aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8 ${selectedOption === 'CHIP' ? 'drop-shadow border-solid border-3 border-purple-700' : ''}`}>
+            <div className={`aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg xl:aspect-w-7 xl:aspect-h-8 ${selectedOption === 'CHIP' ? 'drop-shadow border-solid border-3 border-purple-700' : ''}`}>
               <img
-                src="http://placekitten.com/g/300/300"
+                src="/images/chip_black.png"
                 alt="An NFT TAG"
                 className="h-full w-full object-cover object-center group-hover:opacity-75"
               />
@@ -44,8 +44,12 @@ const PrintModal: FC<any> = ({ nftData, onClose }) => {
           </div>
         </div>
         <div className="flex flex-col w-full gap-y-2 xl:gap-x-4 pb-4">
-          <button disabled={!selectedOption} type="button" className="button w-full mx-auto mb-4 rounded-md px-4 py-2 text-gray-200 bg-purple-700 hover:bg-purple-500 font-semibold">Print Your NFT</button>
-          <button disabled={!selectedOption} type="button" className="button w-full mx-auto mb-4 rounded-md px-4 py-2 text-gray-200 bg-purple-700 hover:bg-purple-500 font-semibold">Use RAMP</button>
+          <button disabled={!selectedOption} type="button" className="button w-full mx-auto mb-4 rounded-md px-4 py-2 text-gray-200 bg-purple-700 hover:bg-purple-500 font-semibold">Create your NFP</button>
+          <button disabled={!selectedOption} type="button" className="button w-full flex flex-row justify-center align-center mx-auto mb-4 rounded-md px-4 py-2 text-gray-200 bg-gray-200 hover:bg-gray-300 font-semibold text-gray-900">
+            Pay with Ramp.Network
+            {' '}
+            <img src="/images/ramp-logo-square-1000.png" alt="Ramp Logo" className="ml-2 h-6" />
+          </button>
         </div>
       </div>
     </Modal>
--- sections/Dashboard.tsx
+++ sections/Dashboard.tsx
@@ -7,50 +7,51 @@ import Modal from '../components/Modal/Modal';
 import PrintModal from '../components/PrintModal/PrintModal';
 
 export const Dashboard = () => {
-  const [userNFTs, setUserNFTs] = useState([]);
-  const [openModal, setOpenModal] = useState(false);
-  const [selectedNFT, setSelectedNFT] = useState(null);
+  const [ userNFTs, setUserNFTs ] = useState([]);
+  const [ openModal, setOpenModal ] = useState(false);
+  const [ selectedNFT, setSelectedNFT ] = useState(null);
   const { getNFTFromWallet } = useAlchemy();
   const { account } = useEthers();
 
   useEffect(() => {
-    const getNfts = async () => account ? await getNFTFromWallet(account) : Promise.resolve(undefined);
+    const getNfts = async () => (account ? await getNFTFromWallet(account) : Promise.resolve(undefined));
 
-    getNfts().then(ans => {
+    getNfts().then((ans) => {
       if (ans) {
         setUserNFTs(ans.ownedNfts.filter((nft: any) => !nft.error));
       }
     });
+  }, [ account ]);
 
-  }, [account])
-
-  const renderNFTs = () => {
-    return (<div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8" >
+  const renderNFTs = () => (
+    <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
       <div className="group relative">
-        {userNFTs.map((nft: any) => <NFTTile key={nft.id.tokenId + nft.timeLastUpdated} nftData={nft} onClick={(nft) => setSelectedNFT(nft)}/>)}
+        {userNFTs.map((nft: any) => <NFTTile key={nft.id.tokenId + nft.timeLastUpdated} nftData={nft} onClick={(nft) => setSelectedNFT(nft)} />)}
       </div>
-    </div >)
-  }
+    </div>
+  );
 
-  return (<SectionTemplate id="dashboard">
-    <div className="relative min-h-screen max-w-6xl mx-auto flex">
-      <div className="mt-16 text-center bg-black">
-        <div className="my-24">
-          <h1 className="text-3xl font-bold text-gray-200 mb-8">
-            Non Fungible
-            {' '}
-            <a className="text-purple-700">PRINTS</a>
-          </h1>
-        </div>
-        <button className="button font-bold text-gray-200 mb-8" onClick={() => setOpenModal(true)}>Open Modal</button>
-        <div>
-          <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-            {renderNFTs()}
+  return (
+    <SectionTemplate id="dashboard">
+      <div className="relative min-h-screen max-w-6xl mx-auto flex">
+        <div className="mt-16 text-center bg-black">
+          <div className="my-24">
+            <h1 className="text-3xl font-bold text-gray-200 mb-8">
+              Non Fungible
+              {' '}
+              <a className="text-purple-700">PRINTS</a>
+            </h1>
+          </div>
+          <div>
+            <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+              {renderNFTs()}
+            </div>
           </div>
         </div>
-      </div>
 
-      {selectedNFT && <PrintModal nftData={selectedNFT} onClose={() => setSelectedNFT(null)}/>}
-    </div>)
-  </SectionTemplate>)
+        {selectedNFT && <PrintModal nftData={selectedNFT} onClose={() => setSelectedNFT(null)} />}
+      </div>
+      )
+    </SectionTemplate>
+  );
 };
