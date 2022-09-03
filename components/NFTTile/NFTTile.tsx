b54b9cb9ff4d3e6315138441c02fceaef04a1805
Kodziak <kodziak1416@gmail.com>
2022-09-04 01:03:34 +0200

    Redesign

--- components/NFTTile/NFTTile.tsx
+++ components/NFTTile/NFTTile.tsx
@@ -1,14 +1,13 @@
 // import Image from "next/image"
-import React, { FC } from "react"
+import React, { FC } from 'react';
 
-const NFTTile: FC<any> = ({ nftData, onClick }) => {
-    return (<div className="group bg-gray-600 rounded-lg cursor-pointer" onClick={() => onClick(nftData)}>
-        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-            <img src={`${nftData.tokenUri.gateway}`} />
-        </div>
-            <h3 className="mt-4 text-sm text-gray-200">{`${nftData.contractMetadata?.name || ''}`}</h3>
-            <p className="mt-1 text-lg font-medium text-gray-300">{`Token ID: ${parseInt(nftData.id.tokenId, 16)}`}</p>
-    </div>)
-}
+const NFTTile: FC<any> = ({ nftData, onClick }) => (
+  <div className="group rounded-lg cursor-pointer border-2 border-gray-200" onClick={() => onClick(nftData)}>
+    <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+      <img src={nftData.tokenUri.gateway} alt={nftData.contractMetadata?.name || ''} />
+    </div>
+    <h3 className="py-4 text-md text-bold text-gray-200">{`${nftData.contractMetadata?.name || ''} #${parseInt(nftData.id.tokenId, 16)}`}</h3>
+  </div>
+);
 
 export default NFTTile;
